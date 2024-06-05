1e3896d03195cfdc17b46098a083d7426b41f7b1
Marie Demarly <marie.idalia.castro@gmail.com>
2024-06-05 21:53:45 +0200

    Added character and camera movement
    Added character movement and cineMachine camera now follows the player.

---
+++ Assets/Scripts/PlayerController/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float movementSpeed = 4f;
+    public MainCameraController MCC;
+    public float rotationSpeed = 600f;
+    Quaternion requiredRotation;
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log("Horizontal: " + horizontal);
+
+        float movementAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+
+        var movementInput = (new Vector3(horizontal, 0, vertical)).normalized;
+
+        var movementDirection = MCC.flatRotation * movementInput;
+
+        if(movementAmount > 0)
+        {
+            transform.position += movementDirection * movementSpeed * Time.deltaTime;
+            requiredRotation = Quaternion.LookRotation(movementDirection);
+
+
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, requiredRotation, rotationSpeed * Time.deltaTime);
+    }
+
+}
