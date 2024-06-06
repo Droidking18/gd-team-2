180e526d48a38fe04e9c15e6dbd46755d45950a8
Marie Demarly <marie.idalia.castro@gmail.com>
2024-06-06 22:13:43 +0200

    Added menu buttons and functionality

---
+++ Assets/Scripts/MenuController/MenuController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    //[Header("Player Movement")]
+
+    public string newGameLevel;
+    private string levelToLoad;
+    [SerializeField] private GameObject DialogNoGameSaved = null;
+    [SerializeField] private GameObject MainMenuContainer = null;
+    [SerializeField] private GameObject DialogExitGame = null;
+
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("OfficeSprint"))
+        {
+            levelToLoad = PlayerPrefs.GetString("OfficeSprint");
+            // To create a level to push into this filed SavedLevel
+            //PlayerPrefs.setString("SavedLevel", yourlevel);
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            // Controls the no saved game dialog appearing
+            DialogNoGameSaved.SetActive(true);
+        }
+    }
+
+    public void ExitButtonYes()
+    {
+        Application.Quit();
+    }
+
+    public void ExitButtonNo()
+    {
+        MainMenuContainer.SetActive(true);
+        DialogExitGame.SetActive(false);
+
+    }
+}
