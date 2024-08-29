e981b5a228dd64a70f5aade8cf66eef62ccbb370
Marie Demarly <71197865+Marieid@users.noreply.github.com>
2024-08-29 18:09:17 +0200

    Game over scene implemented

--- Assets/OfficeSprint/Scripts/GameController/GameController.cs
+++ Assets/OfficeSprint/Scripts/GameController/GameController.cs
@@ -7,12 +7,15 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        countdown.OnTimeExpired += HandleTimeExpired; // Subscribe to the event
+        // Subscribes to the event
+        countdown.OnTimeExpired += HandleTimeExpired; 
     }
 
     private void HandleTimeExpired()
     {
         Debug.Log("Time has expired!");
-        // Add game over logic here
+
+        // Loads Game lost scene
+        SceneManager.LoadScene("YouLoseScene"); 
     }
 }
